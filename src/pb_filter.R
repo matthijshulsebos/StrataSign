9b0144ca71b857457256cc1bd933920c04ef40fd
Matthijs Hulsebos <m.c.hulsebos@outlook.com>
2024-12-29 16:07:08 +0100

    :sparkles: Added conversion to tumor pseudo bulk at patient level

---
+++ src/pb_filter.R
@@ -0,0 +1,28 @@
+# This script filters non-metabolic genes and healthy samples from the pseudo bulk dataset 
+
+library(Matrix)
+
+# Load datasets
+if (!exists("lung_ldm")) {
+  message("Loading Mt. Sinai scRNAseq data into R")
+  load("data/lung_ldm.rd")
+}
+
+pseudo_bulk <- read.csv("src_output/pseudo_bulk.csv",r=1,h=1,stringsAsFactors = F, check.names = FALSE)
+sample_annots <- read.csv("input_tables/table_s1_sample_table.csv",r=1,h=1,stringsAsFactors = F)
+imm_genes <- read.csv("src_output/hsa01100_genes.csv",r=1,h=1,stringsAsFactors = F)
+
+# Retrieve tumor sample metadata
+sample_annots_tumor <- sample_annots[sample_annots$tissue == "Tumor", ]
+
+# Filter expression data on metabolic genes (rows) and tumor samples (columns)
+pseudo_bulk_filtered <- pseudo_bulk[intersect(unlist(imm_genes), rownames(pseudo_bulk)), intersect(rownames(sample_annots_tumor), colnames(pseudo_bulk))]
+
+# Write to file
+write.csv(
+  pseudo_bulk_filtered,
+  file = "src_output/pb_patient_filtered.csv"
+)
+
+# Clear memory
+rm(lung_ldm)
---
+++ src/pb_patient.R
@@ -0,0 +1,29 @@
+# This script converts pseudo bulk data at sample level to patient level
+
+library(Matrix)
+
+# Load the datasets. Use pseudo_bulk.csv if you do not want filtered genes and samples
+pb_sample_filtered <- read.csv("src_output/pseudo_bulk_filtered.csv",r=1,h=1,stringsAsFactors = F, check.names = FALSE)
+sample_annots <- read.csv("input_tables/table_s1_sample_table.csv",r=1,h=1,stringsAsFactors = F)
+
+# Create a group factor linking sample ID and patient ID
+group_labels <- setNames(sample_annots$patient_ID, rownames(sample_annots))
+group_labels <- factor(group_labels)
+
+# Extract grouping variable (patient IDs in the order of the samples)
+grouping <- group_labels[colnames(pb_sample_filtered)]
+
+# Transpose data to [samples x genes] for aggregation
+pb_sample_filtered_t <- t(pb_sample_filtered)
+
+# Aggregate data by patient
+pb_patient_filtered_t <- rowsum(pb_sample_filtered_t, group = grouping)
+
+# Transpose back
+pb_patient_filtered <- t(pb_patient_filtered_t)
+
+# Write to file
+write.csv(
+  pb_patient_filtered,
+  file = "src_output/pb_patient_filtered.csv"
+)
