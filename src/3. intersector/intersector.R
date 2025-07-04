d4dd3c92e50471b9b85e43eb08047e0ee9dd0648
matthijshulsebos <m.c.hulsebos@gmail.com>
2025-07-04 06:27:32 +0200

    :wrench: Set minimum intersector models to 3

--- src/3. intersector/intersector.R
+++ src/3. intersector/intersector.R
@@ -380,7 +380,7 @@ run_intersector <- function(
 run_intersector(
   base_models_dir = "output/2. models",
   base_output_dir = "output/3. intersector",
-  min_models_param = 2,
+  min_models_param = 3,
   cell_types_to_process = c("all_clusters", "macrophages", "lcam_hi", "lcam_lo", "lcam_both"),
   gene_types_to_process = c("metabolic", "nonmetabolic", "random"),
   sublineage_color_map_path = "output/3. intersector/sublineage_colors.rds"
